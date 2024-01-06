34dbc0396a766f4769a35bae162f5d631e137353
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-06 22:59:42 +0800

    pseudocode and setup

---
+++ biggest_number.py
@@ -0,0 +1,10 @@
+# Ask user to input 3 numbers. Find and print the biggest number using only if-else statement.
+
+# pseudocode
+
+import tkinter as tk
+from tkinter import PhotoImage
+
+# setup
+root = tk.Tk()
+root.title("What's the Biggest Number?")
